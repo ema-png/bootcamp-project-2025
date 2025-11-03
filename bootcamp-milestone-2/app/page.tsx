1f9b954a739440b7a6fd13994d197a9259b49fd3
Emma Walker <ewalker@gmail.com>
2025-11-03 12:21:41 -0800

    Organized files and added navbar

---
+++ bootcamp-milestone-2/app/page.tsx
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <div>
+          <h1>
+          </h1>
+          <p>
+            <a>
+            </a>
+            <a>
+            </a>
+          </p>
+        </div>
+        <div>
+          <a>
+          </a>
+          <a>
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
