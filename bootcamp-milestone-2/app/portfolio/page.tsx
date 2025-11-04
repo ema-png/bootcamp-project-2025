53b2bcfb29b9513d0d156be3e944ec72ccbb7d14
Emma Walker <ewalker@gmail.com>
2025-11-04 02:52:27 -0800

    Portfolio component & Contact page

---
+++ bootcamp-milestone-2/app/portfolio/page.tsx
@@ -0,0 +1,10 @@
+import PortfolioEntry from "@/components/portfolioEntry";
+
+export default function Portfolio() {
+  return (
+    <main>
+      <h1 className="pagetitle">₊✩‧₊˚Emma&apos;s Portfolio˚₊✩‧₊</h1>
+      <PortfolioEntry/>
+    </main>
+  );
+}
--- bootcamp-milestone-2/components/portfolioEntry.tsx
+++ bootcamp-milestone-2/components/portfolioEntry.tsx
@@ -1,26 +1,27 @@
 import React from "react";
-import style from "./portfolio.module.css";
-/*mport { PortfolioEntry } from "../app/portfolioEntries"*/
+import style from "./portfolioEntry.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Portfolio(){
+export default function PortfolioEntry(){
     return(
         <div className = {style.project}>
             <div className = {style.projectimage}>
-                <Link href="index.html">
-                    <Image src="./personalwebsite.png" alt="emma's website" width = {700} height = {700}>
+                <Link href="/">
+                    <Image src="/personalwebsite.png" alt="emma's website" width = {700} height = {700}>
                     </Image>
                 </Link>
             </div>
             <div className = {style.projectdetails}>
                 <p className = {style.projectname}>
-                    <h2> <strong> Personal Website </strong> </h2> 
+                     Personal Website
                 </p>
                 <p className = {style.projectdescription}>
-                    Project for Hack4Impact application, made using HTML and CSS
+                    Project for Hack4Impact application, made using HTML, CSS, and React
                 </p>
-                <Link href="/">click me to see the project!</Link>
+                <p className = {style.projectLink}>
+                <Link href="/">click me to view!</Link>
+                </p>
             </div>
         </div>
     )
